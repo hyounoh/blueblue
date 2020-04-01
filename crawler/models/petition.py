3005be345e3dc4d2b8c2a215bac0c517ce208aa9
Hyounoh <hyounohshim@gmail.com>
2020-04-01 16:17:38 +0900

    [ENH] Add models added models are petitionMeta, petitionContent, word. these are used to save crawled documents and change tagger module directory name

---
+++ crawler/models/petition.py
@@ -0,0 +1,90 @@
+class PetitionMeta:
+
+    def __init__(self, no, category, n_participants, date_expired, date_start, detail_url, title):
+        self.__no = no
+        self.__category = category
+        self.__n_participants = n_participants
+        self.__date_expired = date_expired
+        self.__date_start = date_start
+        self.__detail_url = detail_url
+        self.__title = title
+
+    def __del__(self):
+        pass
+
+    @property
+    def no(self):
+        return self.__no
+
+    @property
+    def category(self):
+        return self.__category
+
+    @property
+    def n_participants(self):
+        return self.__n_participants
+
+    @property
+    def date_expired(self):
+        return self.__date_expired
+
+    @property
+    def date_start(self):
+        return self.__date_start
+
+    @property
+    def detail_url(self):
+        return self.__detail_url
+
+    @property
+    def title(self):
+        return self.__title
+
+
+class PetitionContent:
+
+    def __init__(self, petition_id, text):
+        self.__petition_id = petition_id
+        self.__text = text
+
+    def __del__(self):
+        pass
+
+    @property
+    def petition_id(self):
+        return self.__petition_id
+
+    @property
+    def text(self):
+        return self.__text
+
+
+class Word:
+
+    def __init__(self, text, morpheme, petition_id, position):
+        self.__text = text
+        self.__morpheme = morpheme
+        self.__petition_id = petition_id
+        self.__position = position
+
+    def __del__(self):
+        pass
+
+    def __str__(self):
+        return self.__text, self.__morpheme, self.__petition_id, self.__position
+
+    @property
+    def text(self):
+        return self.__text
+
+    @property
+    def morpheme(self):
+        return self.__morpheme
+
+    @property
+    def petition_id(self):
+        return self.__petition_id
+
+    @property
+    def position(self):
+        return self.__position
