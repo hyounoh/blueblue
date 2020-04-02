b5e63efeec4a2b12236c7573c6892fb341047ba9
Hyounoh <hyounohshim@gmail.com>
2020-04-02 17:44:56 +0900

    [ENV] Set logger

--- crawler/main.py
+++ crawler/main.py
@@ -1,13 +1,23 @@
 import time
 import schedule
+import logging
 
 from src.crawler import crawling
 
 
 if __name__ == "__main__":
+
+    # Set logger
+    logger = logging.getLogger("blueblue")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
     schedule.every().day.at("12:00").do(crawling)
 
     while True:
-        print("Check crawling schedule...")
+        logger.info("Check crawling schedule...")
         schedule.run_pending()
         time.sleep(10)
