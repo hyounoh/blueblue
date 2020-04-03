1fbe4e8bd329caeb70733d52933a149ff92a3db0
Hyounoh <hyounohshim@gmail.com>
2020-04-03 13:53:40 +0900

    [ENV] Initialize api with flask template

---
+++ api/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restful_swagger_2 import Api
+
+from src.apiresourcename import ApiResourceName
+
+app = Flask(__name__)
+
+# setting CORS
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+# setting swagger api doc
+api = Api(app, title='API Template', api_version='0.0.1', api_spec_url='/swagger', host='localhost', description='API Template')
+
+# add resources
+# ex) http://localhost:5000/api_resource_name
+api.add_resource(ApiResourceName, '/api_resource_name')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, threaded=True, debug=True)
