6208835e6e335da985762ede77524872e1f88c47
Hyounoh <hyounohshim@gmail.com>
2020-04-17 17:48:36 +0900

    [ENH] Add recent top 3 word on dashboard use recentword endpoint

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Dashboard.css';
+import axios from 'axios'
 
 const Dashboard = () => {
+
+    // Define state of wordcloud data
+    const [words, setWords] = useState('');
+
+    // Set event on refresh to load wordcloud data
+    const onRefresh = () => {
+        axios.get('http://localhost:5001/recentword?use_stopword=1')
+            .then(
+                response => {
+                    let words = response.data['results']
+                    console.log(words)
+
+                    setWords(words);
+                }
+            )
+            .catch(
+                response => {
+                    console.log(response)
+                }
+            );
+    };
+
+    // Load wordcloud when this page is rendered.
+    useEffect(() => {
+        console.log('rendered!');
+        onRefresh();
+    }, []);
+
     return (
         <div className="Dashboard">
             <div className="DashboardItem Word">
@@ -9,7 +38,7 @@ const Dashboard = () => {
                     최근 일주일 핵심 단어
                 </div>
                 <div className="DashboardItemContent">
-                    코로나, 정부, 국민
+                    {words}
                 </div>
             </div>
             <div className="DashboardItem Count">
