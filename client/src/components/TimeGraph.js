27cc27e0c32b43d8cbd56221c16b6adc1558a6ca
Hyounoh <hyounohshim@gmail.com>
2020-04-22 15:41:52 +0900

    [ENH] Add petition count chart by using responsive bar from nivo

--- client/src/components/TimeGraph.js
+++ client/src/components/TimeGraph.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import '../css/TimeGraph.css'
 import '../../node_modules/react-vis/dist/style.css';
-import { XYPlot, VerticalBarSeries, XAxis } from 'react-vis';
 import RefreshIcon from '../icons/round_refresh_black_18dp.png';
 import axios from 'axios'
+import { ResponsiveBar } from '@nivo/bar'
 
 const TimeGraph = () => {
 
     // Define state of wordcloud data
-    const [graph, setGraph] = useState([{ x: 0, y: 0 }]);
+    const [graph, setGraph] = useState([{ "date": "0000-00-00", "count": 0 }]);
 
     // Set event on refresh to load wordcloud data
     const onRefresh = () => {
@@ -17,10 +17,7 @@ const TimeGraph = () => {
                 response => {
                     let graph = response.data['results']['graph'];
                     console.log(graph)
-                    let graph_formatted = graph.map((item) => ({ 'x': item['date'], 'y': item['count'] }))
-                    console.log(graph_formatted);
-
-                    setGraph(graph_formatted);
+                    setGraph(graph);
                 }
             )
             .catch(
@@ -41,10 +38,57 @@ const TimeGraph = () => {
             <div className="Refresh" onClick={onRefresh}>
                 <img src={RefreshIcon} alt="Refresh"></img>
             </div>
-            <XYPlot height={500} width={1300} xType="ordinal">
-                <XAxis />
-                <VerticalBarSeries data={graph} />
-            </XYPlot>
+            <div className="PlotContainer">
+                <ResponsiveBar
+                    data={graph}
+                    keys={['count']}
+                    indexBy="date"
+                    margin={{ top: 64, right: 128, bottom: 64, left: 64 }}
+                    padding={0.3}
+                    // colors={{ scheme: 'nivo' }}
+                    colors={'#082E59'}
+                    borderColor={{ from: 'color', modifiers: [['darker', -1.6]] }}
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: -45,
+                        legendOffset: 32
+                    }}
+                    axisLeft={null}
+                    labelSkipWidth={12}
+                    labelSkipHeight={12}
+                    labelTextColor={{ from: 'color', modifiers: [['darker', -50]] }}
+                    legends={[
+                        {
+                            dataFrom: 'keys',
+                            anchor: 'bottom-right',
+                            direction: 'column',
+                            justify: false,
+                            translateX: 120,
+                            translateY: 0,
+                            itemsSpacing: 2,
+                            itemWidth: 100,
+                            itemHeight: 20,
+                            itemDirection: 'left-to-right',
+                            itemOpacity: 0.85,
+                            symbolSize: 20,
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                    animate={true}
+                    motionStiffness={90}
+                    motionDamping={15}
+                />
+            </div>
         </div>
     )
 }
