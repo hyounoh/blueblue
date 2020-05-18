b77e2fde7b69d2954a4720a674301353e210435c
Hyounoh <hyounohshim@gmail.com>
2020-05-18 14:28:21 +0900

    [ENH] Add stopword area into admin page

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "../../css/admin/Admin.css";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className="Admin-wrapper">
+      <div className="Title">Admin</div>
+      <div className="Stopword-wrapper">
+        <div className="Stopword-title">Stopword-title</div>
+        <div className="Stopword-content">Stopword-list</div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
