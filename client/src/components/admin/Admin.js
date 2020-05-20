6bc87478a3f9c62b9e12f565c20b0432f9a70043
Hyounoh <hyounohshim@gmail.com>
2020-05-20 10:43:57 +0900

    [ENH] Make Stopword component in Admin It only has a read function now. will be added create, update, delete soon.

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../../css/admin/Admin.css";
+import Stopword from "./Stopword";
 
 const Admin = () => {
   return (
     <div className="Admin-wrapper">
       <div className="Title">Admin</div>
-      <div className="Stopword-wrapper">
-        <div className="Stopword-title">Stopword-title</div>
-        <div className="Stopword-content">Stopword-list</div>
+      <div>
+        <Stopword></Stopword>
       </div>
     </div>
   );
