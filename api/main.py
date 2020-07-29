9947488d8ff2e83cd53c3ee6acfc346bd3294e31
Hyounoh <hyounohshim@gmail.com>
2020-07-29 15:59:15 +0900

    [CLN]

--- api/main.py
+++ api/main.py
@@ -4,23 +4,22 @@ from flask_restful_swagger_2 import Api
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-from src import *
 from src.word import *
 from src.petition import *
 
 app = Flask(__name__)
 
-# setting CORS
+# Set CORS
 cors = CORS(app, resources={r'*': {'origins': '*'}})
 
+# Set limiter
 limiter = Limiter(app, key_func=get_remote_address, default_limits=["100 per day"])
 
-# setting swagger api doc
+# Set swagger
 api = Api(app, title='API Template', api_version='0.0.1', api_spec_url='/swagger', host='localhost',
           description='API Template')
 
-# add resources
-# ex) http://localhost:5000/api_resource_name
+# Add resources
 api.add_resource(Status, '/status')
 api.add_resource(WordCloud, '/wordcloud')
 api.add_resource(Graph, '/petition-graph')
